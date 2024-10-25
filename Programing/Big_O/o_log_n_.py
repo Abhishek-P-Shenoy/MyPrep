9cf29e67241720e7825c1e2ecbcd6896007ed1b7
AbGuru <116103254+Abhishek-P-Shenoy@users.noreply.github.com>
2024-10-25 17:53:05 +0530

    change

--- Programing/Big_O/o_log_n_.py
+++ Programing/Big_O/o_log_n_.py
@@ -4,5 +4,13 @@ here O(log(n)) is a logorithmic fucntion
 
 '''
 
-# example for this will be finding an element in a sorted array by spliting it
+# example for this will be searching/finding an element in a sorted array by spliting it
+
+'''
+
+we also have O(n*Log(n)) 
+
+'''
+
+# example for this would be in merge sort algo
 
