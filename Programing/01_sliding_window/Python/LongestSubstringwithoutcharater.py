ae98d5ac13eb19f48b3a4b67f80f59e9691e7691
AbGuru <116103254+Abhishek-P-Shenoy@users.noreply.github.com>
2024-10-25 17:27:28 +0530

    Basic big o concepts and basic folders

---
+++ Programing/01_sliding_window/Python/LongestSubstringwithoutcharater.py
@@ -0,0 +1,28 @@
+'''
+Problem
+input --> ABCABABCD
+output--> ABCD
+
+
+All possibale solution:
+1. Substring
+    two pointer
+    sliding Window
+    hash map
+2. longest 
+3. Without repeating character
+
+Not usefull:
+sorting is not possible, so 2 pointer cant be used, substring i.e we need the placment is needed so two pointer cant be used.
+solution :
+sliding window with largest window
+
+I x----------x J
+in sliding window we will keep i and keep moving j that is growing the window with a role here we need to role from problem
+
+here condition can be checked by saving it 
+    hashmap  
+    character array
+and maintaing the count of each
+
+'''
---
+++ Programing/Big_O/o_n_.py
@@ -0,0 +1,13 @@
+'''
+O(n) is Linear time complexity condition 
+
+example for this is running is single for loop in a fucntion then the fucntion is said to be O(n)
+'''
+
+def print_items(n):
+    for i in range(n):
+        print(i)
+
+print_items(10)
+
+# this runs n times that is the size of the given items
